ded998506910b8e6dc6a5281d738b18e911e85d6
unknown <muh.it.eng.19841984@gmail.com>
2022-08-15 23:27:47 +0300

    structure

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
-const express=require('express');
-const app=express()
-const PORT=3000;
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
 
-app.get('/',(req,res)=>{
-    res.send('ok');
-})
-app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}`);
-})
+dotenv.config({ path: 'config.env' });
+const dbConnection = require('./config/database');
+const categoryRoute = require('./routes/categoryRoute');
+
+// Connect with db
+dbConnection();
+
+// express app
+ const app = express();
+
+ // Middlewares
+ app.use(express.json());
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+  console.log(`mode: ${process.env.NODE_ENV}`);
+}
+
+// Mount Routes
+app.use('/api/v1/categories', categoryRoute);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`App running running on port ${PORT}`);
+});
