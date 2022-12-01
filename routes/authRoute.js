5dfe0631197af5147a4f2558097c11ff5cac957f
unknown <muh.it.eng.19841984@gmail.com>
2022-12-01 18:09:38 +0200

    signup and login routes

---
+++ routes/authRoute.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const {
+  signupValidator,
+  loginValidator,
+} = require("../utils/validators/authValidator");
+
+const { signup, login } = require("../services/authService");
+
+const router = express.Router();
+
+router.post("/signup", signupValidator, signup);
+router.post("/login", loginValidator, login);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const subCategoryRoute = require("./routes/subCategoryRoute");
 const brandRoute = require("./routes/brandRoute");
 const productRoute = require("./routes/productRoute");
 const userRoute = require("./routes/userRoute");
+const authRoute = require("./routes/authRoute");
 //error
 const globalError = require("./middlewares/errorMiddleware");
 
@@ -38,6 +39,7 @@ app.use("/api/v1/subcategories", subCategoryRoute);
 app.use("/api/v1/brands", brandRoute);
 app.use("/api/v1/products", productRoute);
 app.use("/api/v1/users", userRoute);
+app.use("/api/v1/auth", authRoute);
 
 app.all("*", (req, res, next) => {
   // const err=new Error(`rout not found : ${req.originalUrl}`);
