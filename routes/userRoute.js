c013c1ed6062f032c51814163e2a7e153406c771
unknown <muh.it.eng.19841984@gmail.com>
2022-12-02 13:16:04 +0200

    CRUD logged user

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,6 +6,7 @@ const {
   updateUserValidator,
   deleteUserValidator,
   changeUserPasswordValidator,
+  updateLoggedUserValidator,
 } = require("../utils/validators/userValidator");
 
 const {
@@ -17,50 +18,64 @@ const {
   uploadUserImage,
   resizeImage,
   changeUserPassword,
+  getLoggedUserData,
+  updateLoggedUserPassword,
+  updateLoggedUserData,
+  deleteLoggedUserData,
 } = require("../services/userService");
 
 const authService = require("../services/authService");
 
 const router = express.Router();
 
+router.use(authService.protect);
+
+router.get("/getMe", getLoggedUserData, getUser);
+router.put("/changeMyPassword", updateLoggedUserPassword);
+router.put("/updateMe", updateLoggedUserValidator, updateLoggedUserData);
+router.delete("/deleteMe", deleteLoggedUserData);
+
+// Admin
+router.use(authService.allowedTo("admin", "manager"));
 router.put(
   "/changePassword/:id",
   changeUserPasswordValidator,
   changeUserPassword
 );
-
 router
   .route("/")
-  .get(authService.protect, authService.allowedTo("admin", "manager"), getUsers)
-  .post(
-    authService.protect,
-    authService.allowedTo("admin"),
-    uploadUserImage,
-    resizeImage,
-    createUserValidator,
-    createUser
-  );
+  .get(getUsers)
+  .post(uploadUserImage, resizeImage, createUserValidator, createUser);
 router
   .route("/:id")
-  .get(
-    authService.protect,
-    authService.allowedTo("admin"),
-    getUserValidator,
-    getUser
-  )
-  .put(
-    authService.protect,
-    authService.allowedTo("admin"),
-    uploadUserImage,
-    resizeImage,
-    updateUserValidator,
-    updateUser
-  )
-  .delete(
-    authService.protect,
-    authService.allowedTo("admin"),
-    deleteUserValidator,
-    deleteUser
-  );
+  .get(getUserValidator, getUser)
+  .put(uploadUserImage, resizeImage, updateUserValidator, updateUser)
+  .delete(deleteUserValidator, deleteUser);
+
+module.exports = router;
+
+router.use(authService.protect);
+
+router.get("/getMe", getLoggedUserData, getUser);
+router.put("/changeMyPassword", updateLoggedUserPassword);
+router.put("/updateMe", updateLoggedUserValidator, updateLoggedUserData);
+router.delete("/deleteMe", deleteLoggedUserData);
+
+// Admin
+router.use(authService.allowedTo("admin", "manager"));
+router.put(
+  "/changePassword/:id",
+  changeUserPasswordValidator,
+  changeUserPassword
+);
+router
+  .route("/")
+  .get(getUsers)
+  .post(uploadUserImage, resizeImage, createUserValidator, createUser);
+router
+  .route("/:id")
+  .get(getUserValidator, getUser)
+  .put(uploadUserImage, resizeImage, updateUserValidator, updateUser)
+  .delete(deleteUserValidator, deleteUser);
 
 module.exports = router;
